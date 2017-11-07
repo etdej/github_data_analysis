cd3b599340d25d928c735cd9ca31544e74997013
Jason Phang <email@jasonphang.com>
2017-11-06 23:48:04 -0500

    Print DataFrame

--- data/sample.py
+++ data/sample.py
@@ -6,7 +6,7 @@ url = 'http://data.githubarchive.org/2015-05-22-12.json.gz'
 raw_data_ls = grab_data.get_raw_data(url)
 output = grab_data.extract_data(raw_data_ls)
 
-pd.DataFrame({
+print(pd.DataFrame({
     k1: {k2: len(v2) for k2, v2 in v1.items()}
     for k1, v1 in output.items()
-}).T
+}).T)
